20404da4bb7982f4e4566fd484123ab726ef6b2f
Ivan Karnaukhov <iv.karnaukhov@gmail.com>
2025-07-13 23:35:54 +0300

    fix(cors): allow vercel domain

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
+    "https://tg-screenfree.vercel.app",
 ]
 app.add_middleware(
     CORSMiddleware,
